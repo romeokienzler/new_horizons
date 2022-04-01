58bf5a13ebced0de5c8e483f41b9c7ab985d3754
Romeo Kienzler <romeokienzler@li-c7445bcc-23db-11b2-a85c-f854e76b2a39.ibm.com>
2022-04-01 16:33:35 +0200

    add post kubeflow installation

--- ubuntu_install_new_horizons_kind.sh
+++ ubuntu_install_new_horizons_kind.sh
@@ -5,6 +5,7 @@ service docker start
 ./kind/install_kind.sh
 cd kind
 ./create_kind_cluster.sh
+./enable_ingress_on_kind.sh
 cd ..
 kubectl cluster-info --context kind-kind
 ./kubeflow/install_kubeflow.sh

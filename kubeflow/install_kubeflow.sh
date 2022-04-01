8d2b313626a9b15237665730e6e32106c8e036c3
Romeo Kienzler <romeokienzler@li-c7445bcc-23db-11b2-a85c-f854e76b2a39.ibm.com>
2022-04-01 19:06:58 +0200

    add rbac

--- kubeflow/install_kubeflow.sh
+++ kubeflow/install_kubeflow.sh
@@ -5,3 +5,4 @@ git clone https://github.com/kubeflow/manifests.git
 cd manifests
 while ! kustomize build example | kubectl apply -f -; do echo "Retrying to apply resources"; sleep 10; done
 popd
+kubectl apply -n kubeflow -f rbac.yaml

049dfb5ccefd20c5b6ecd8cf9d24ab2104b2b090
Romeo Kienzler <romeokienzler@li-c7445bcc-23db-11b2-a85c-f854e76b2a39.ibm.com>
2022-04-01 16:01:49 +0200

    automate on ubuntu and kind

---
+++ ubuntu_install_new_horizons_kind.sh
@@ -0,0 +1,7 @@
+./ubuntu_install_docker.sh
+./install_kubectl.sh
+./install_kustomize.sh
+./kind/install_kind.sh
+./kind/create_kind_cluster.sh
+kubectl config current-context
+./kubeflow/install_kubeflow.sh

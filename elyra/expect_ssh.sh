711605fa48e66b8a3bcf58540a6340c804ac957f
Ubuntu <ubuntu@rkie-container-build.IBM.cloud>
2022-08-18 10:07:29 +0000

    fix add ssh key

--- elyra/expect_ssh.sh
+++ elyra/expect_ssh.sh
@@ -2,3 +2,6 @@
 spawn ssh-add /home/jovyan/.ssh/id_rsa
 expect "Enter passphrase for /home/jovyan/.ssh/id_rsa:"
 send "$env(SSH_PASSWORD)\n"
+expect "Identity added: /home/ubuntu/.ssh/id_rsa (/home/jovyan/.ssh/id_rsa)"
+exit
+

35793d2e5289ac83ce44f1afe65d7a12637409bb
Romeo Kienzler <romeo.kienzler@gmail.com>
2022-09-19 15:26:39 +0200

    Update build.sh

--- elyra/build.sh
+++ elyra/build.sh
@@ -1,4 +1,4 @@
-docker build -t elyra-ce:0.12 .
-docker tag elyra-ce:0.12 romeokienzler/elyra-ce:0.12
-docker push romeokienzler/elyra-ce:0.12
+docker build -t elyra-ce:0.13 .
+docker tag elyra-ce:0.13 romeokienzler/elyra-ce:0.13
+docker push romeokienzler/elyra-ce:0.13
 

607e66ac6146993596128e89d8b39fd21c8ff9a4
Romeo Kienzler <romeo.kienzler@ch.ibm.com>
2022-09-20 10:45:06 +0200

    push image version

--- elyra/build.sh
+++ elyra/build.sh
@@ -1,4 +1,4 @@
-docker build -t elyra-ce:0.13 .
-docker tag elyra-ce:0.13 romeokienzler/elyra-ce:0.13
-docker push romeokienzler/elyra-ce:0.13
+docker build -t elyra-ce:0.14 .
+docker tag elyra-ce:0.14 romeokienzler/elyra-ce:0.14
+docker push romeokienzler/elyra-ce:0.14
 

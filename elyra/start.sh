7d9182ddc969675b8ccbd57b3f488a119204df68
Romeo Kienzler <romeo.kienzler@ch.ibm.com>
2022-09-22 10:32:09 +0200

    create generic image for codeengine and iks

--- elyra/start.sh
+++ elyra/start.sh
@@ -1,34 +1,47 @@
 #!/bin/sh
 
+export S3_SYNC_ENABLED=${S3_SYNC_ENABLED=0}
+if [ $S3_SYNC_ENABLED -eq 1 ]; then
 
-echo AWSAccessKeyId=$ACCESS_KEY_ID >> /home/jovyan/.duplicity_credentials.txt
-echo AWSSecretKey=$SECRET_ACCESS_KEY >> /home/jovyan/.duplicity_credentials.txt
+    echo AWSAccessKeyId=$ACCESS_KEY_ID >> /home/jovyan/.duplicity_credentials.txt
+    echo AWSSecretKey=$SECRET_ACCESS_KEY >> /home/jovyan/.duplicity_credentials.txt
+    export DISABLE_BACKUP_LOOP=${DISABLE_BACKUP_LOOP=0}
+    export PROJECT=${PROJECT="default"}
 
-export DISABLE_BACKUP_LOOP=${DISABLE_BACKUP_LOOP=0}
-export PROJECT=${PROJECT="default"}
+    duplicity list-current-files s3://$S3_ENDPOINT/$BUCKET/$PROJECT
+    export PROJECT_EXISTS=$?
+    if [ $PROJECT_EXISTS -gt 0 ]; then # doesn't exist or other error
+      ssh-keygen -t rsa -N $SSH_PASSWORD -C "jupyter_in_space" -f /home/jovyan/.ssh/id_rsa
+      duplicity --allow-source-mismatch --exclude /home/jovyan/.cache /home/jovyan/ s3://$S3_ENDPOINT/$BUCKET/$PROJECT/
+    else #restore
+      duplicity  restore --force s3://$S3_ENDPOINT/$BUCKET/$PROJECT/ /home/jovyan/
+    #  chmod 400 /home/jovyan/.ssh/id_rsa
+    fi
+    
+    
+    if [ $DISABLE_BACKUP_LOOP -ne 1 ]; then
+      while true; do duplicity --allow-source-mismatch --exclude /home/jovyan/.cache /home/jovyan/ s3://$S3_ENDPOINT/$BUCKET/$PROJECT/; sleep 1; done  &
+    fi
+
+fi
+
+
+
+
 export SSH_PASSWORD=${SSH_PASSWORD="password"}
 export JL_PASSWORD=${JL_PASSWORD="password"}
 
+export GIT_EMAIL=${GIT_EMAIL="gitname"}
+export GIT_NAME=${GIT_NAME="git@e.mail"}
+
 eval `ssh-agent`
 
-duplicity list-current-files s3://$S3_ENDPOINT/$BUCKET/$PROJECT
-export PROJECT_EXISTS=$?
-if [ $PROJECT_EXISTS -gt 0 ]; then # doesn't exist or other error
-  ssh-keygen -t rsa -N $SSH_PASSWORD -C "jupyter_in_space" -f /home/jovyan/.ssh/id_rsa
-  duplicity --allow-source-mismatch --exclude /home/jovyan/.cache /home/jovyan/ s3://$S3_ENDPOINT/$BUCKET/$PROJECT/
-else #restore
-  duplicity  restore --force s3://$S3_ENDPOINT/$BUCKET/$PROJECT/ /home/jovyan/
-#  chmod 400 /home/jovyan/.ssh/id_rsa
-fi
+
 
 ./expect_ssh.sh
 git config --global user.email $GIT_EMAIL
 git config --global user.name $GIT_NAME
 
-
-if [ $DISABLE_BACKUP_LOOP -ne 1 ]; then
-  while true; do duplicity --allow-source-mismatch --exclude /home/jovyan/.cache /home/jovyan/ s3://$S3_ENDPOINT/$BUCKET/$PROJECT/; sleep 1; done  &
-fi
 
 cd /home/jovyan/work/
 jupyter lab --no-browser --ServerApp.password="$(echo $JL_PASSWORD | python -c 'from notebook.auth import passwd;print(passwd(input()))')"

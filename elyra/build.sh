f8b4e2ae3911fcffaeb6dcf35775d2ae44d64a4b
Romeo Kienzler <romeo.kienzler@ch.ibm.com>
2022-09-23 22:44:30 +0200

    add curl

--- elyra/build.sh
+++ elyra/build.sh
@@ -1,4 +1,4 @@
-export IMAGE_VERSION="0.24"
+export IMAGE_VERSION="0.25"
 docker build -t elyra:$IMAGE_VERSION .
 docker tag elyra:$IMAGE_VERSION romeokienzler/elyra:$IMAGE_VERSION
 docker push romeokienzler/elyra:$IMAGE_VERSION

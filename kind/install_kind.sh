77216c79afa63e99c2d4ac8ea551038357eb415c
Romeo Kienzler <romeo.kienzler@gmail.com>
2023-01-26 11:02:07 +0100

    Update install_kind.sh

--- kind/install_kind.sh
+++ kind/install_kind.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -Lo /bin/kind https://github.com/kubernetes-sigs/kind/releases/download/v0.12.0/kind-linux-amd64
+curl -Lo /bin/kind https://github.com/kubernetes-sigs/kind/releases/download/v0.17.0/kind-linux-amd64
 chmod 755 /bin/kind
